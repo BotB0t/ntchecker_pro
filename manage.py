c67d33c858fb9792164dfa405525f4dd5d68a1b5
Antonio Abril <aabrilfl@mercadona.es>
2020-03-08 15:22:30 +0100

    Librarys installed and openshift folder created.

--- manage.py
+++ manage.py
@@ -26,7 +26,7 @@ def main():
     # This allows easy placement of apps within the interior
     # back_middleware directory.
     current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "back_middleware"))
+    sys.path.append(os.path.join(current_path, "ntchecker"))
     execute_from_command_line(sys.argv)
 
 
