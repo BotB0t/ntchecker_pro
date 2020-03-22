b712686018280ceb00061652f3c1b2ab53ed82b3
Antonio Abril <aabrilfl@mercadona.es>
2020-03-22 21:26:19 +0100

    - Fixed list of devices to fit the screen

--- validator_notification/frontend/src/components/devices/Devices.js
+++ validator_notification/frontend/src/components/devices/Devices.js
@@ -24,6 +24,7 @@ export class Devices extends Component {
               <th>NOMBRE</th>
               <th>PLATAFORMA</th>
               <th>PROPIETARIO</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
