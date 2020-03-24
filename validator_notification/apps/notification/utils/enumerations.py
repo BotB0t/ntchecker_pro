fa26626e670282ec9e62c9d728665b3988f34eb3
Antonio Abril <aabrilfl@mercadona.es>
2020-03-24 09:14:04 +0100

    - Added back update model individual notification

--- validator_notification/apps/notification/utils/enumerations.py
+++ validator_notification/apps/notification/utils/enumerations.py
@@ -4,3 +4,9 @@ from validator_notification.utils.enumerations.base import BaseEnum
 class options_notification(BaseEnum):
     OK = 'SI'
     KO = 'NO'
+
+
+class status_notification(BaseEnum):
+    READ = 'READ'
+    NEW = 'NEW'
+    OPENED = 'OPENED'
