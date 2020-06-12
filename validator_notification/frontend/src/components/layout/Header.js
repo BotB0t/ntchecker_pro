6fb5d71d10eb767c31fb1a32695cc4926ba5c42c
Fernando Morella Moya <fernandomorellamoya@gmail.com>
2020-06-12 15:28:33 +0200

    Feat/admin login modal and user dropdown menu (#67)
    * feat/delete-notification-confirmation-modal: Implemented a confirmation modal that appears when the user wants to delete a notification
    
    * refactor/delete-notification-confirmation-modal: Better code formatting and removed the quotes from the message
    
    * feat/admin-dropdown-menu: Added a dropdown menu on the navbar and formatted the navbar
    
    * feat/admin-login-modal-and-user-dropdown: Implemented a dropdown menu on the username that shows a button for admin login if the user is a super user
    
    * refractor/admin-login-modal-and-user-dropdown-menu: Code formatting and optimization. Fixed a problem with the delete notification modal
    
    Co-authored-by: Fernando <ferni_n1@hotmail.com>
    Co-authored-by: Antonio Abril <aabrilfl@mercadona.es>

--- validator_notification/frontend/src/components/layout/Header.js
+++ validator_notification/frontend/src/components/layout/Header.js
@@ -13,11 +13,35 @@ export class Header extends Component {
   render() {
     const { isAuthenticated, user } = this.props.auth;
 
+    const adminButton = (
+      <button
+        className="btn btn-link dropdown-item"
+        data-toggle="modal"
+        data-target="#adminModal"
+      >
+        Entrar como administrador
+      </button>
+    );
+
     const authLinks = (
       <ul className="navbar-nav float-left mt-2 mt-lg-0">
-        <span className="navbar-text mr-3">
-          <strong>{user ? `${user.username}` : ""}</strong>
-        </span>
+        <li className="nav-item dropdown pr-5">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="userDropdown"
+            role="button"
+            data-toggle="dropdown"
+            data-display="static"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <strong>{user ? `${user.username}` : ""}</strong>
+          </a>
+          <div className="dropdown-menu dropdown-menu-right dropdown-menu-left">
+            {user ? (user.is_staff ? adminButton : "") : ""}
+          </div>
+        </li>
         <li className="nav-item">
           <button
             onClick={this.props.logout}
@@ -32,7 +56,7 @@ export class Header extends Component {
     const adminLinks = (
       <li className="nav-item dropdown pr-5">
         <a
-          class="nav-link dropdown-toggle"
+          className="nav-link dropdown-toggle"
           href="#"
           id="navbarDropdown"
           role="button"
@@ -70,45 +94,95 @@ export class Header extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
-        <a className="navbar-brand" href="#">
-          Validador de Notificaciones
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
+          <a className="navbar-brand" href="#">
+            Validador de Notificaciones
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Listado Notificaciones
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/profile-info/devices" className="nav-link">
+                  Listado Dispositivos
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="nav-link">
+                  Contáctanos
+                </Link>
+              </li>
+              {user ? (user.is_staff ? adminLinks : "") : ""}
+
+              {isAuthenticated ? authLinks : guestLinks}
+            </ul>
+          </div>
+        </nav>
+
+        <div
+          className="modal fade"
+          id="adminModal"
+          role="dialog"
+          aria-labelledby="adminModalLabel"
+          aria-hidden="true"
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Listado Notificaciones
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/profile-info/devices" className="nav-link">
-                Listado Dispositivos
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="nav-link">
-                Contáctanos
-              </Link>
-            </li>
-            {user ? (user.is_staff ? adminLinks : "") : ""}
-
-            {isAuthenticated ? authLinks : guestLinks}
-          </ul>
+          <div className="modal-dialog " role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title" id="adminModalLabel">
+                  Acceder como administrador
+                </h4>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <label>Contraseña</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      data-dismiss="modal"
+                    >
+                      Entrar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </nav>
+      </div>
     );
   }
 }
---
+++ validator_notification/frontend/src/components/layout/Modal.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+
+export class Modal extends Component {
+  handleDeleteNotification = () => {
+    this.props.deleteNotification(this.props.id, this.props.title);
+  };
+
+  handleCloseModal = () => {
+    this.props.deactivateModal();
+  };
+
+  render() {
+    return (
+      <div
+        className="modal fade"
+        id="deleteModal"
+        role="dialog"
+        aria-labelledby="deleteModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog " role="document">
+          <div className="modal-content">
+            <div className="modal-header bg-danger">
+              <h4 className="modal-title text-light" id="deleteModalLabel">
+                <span className="display-4">&#9888;</span>
+                <span className="pl-4 font-weight-bold">
+                  ¿Desea borrar esta notificación?
+                </span>
+                <p className="font-italic h5">
+                  Borrar la notificación general supone la pérdida de toda la
+                  información asocidada a esta
+                </p>
+              </h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={this.handleCloseModal}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <h5>
+                <span className="modal-lable font-weight-bold pr-2">
+                  TITULO:&nbsp;
+                </span>
+                {this.props.title}
+              </h5>
+              <h5>
+                <span className="modal-lable font-weight-bold pr-2">
+                  FECHA:&nbsp;
+                </span>
+                {new Intl.DateTimeFormat("es-ES", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                }).format(new Date(this.props.date))}
+              </h5>
+              <br />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+                onClick={this.handleCloseModal}
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={this.handleDeleteNotification}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
